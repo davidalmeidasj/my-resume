15e4d4972bc74dbe29fbe5c4dd6ab0edc33a09e8
davidalmeidasj <davidasj21@gmail.com>
2025-07-15 18:41:15 -0300

    chore(lenguage): change default language

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,7 @@ import pt from './locales/pt.json'
 import en from './locales/en.json'
 import es from './locales/es.json'
 export const i18n = createI18n({
-  locale: 'pt',
-  fallbackLocale: 'en',
+  locale: 'en',
+  fallbackLocale: 'pt',
   messages: { pt, en, es }
 })
